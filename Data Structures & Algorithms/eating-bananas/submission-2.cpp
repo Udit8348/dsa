af6caa753d555d7b15fb9434dcd8bde8a957e45c
Udit8348 <usubramanya3@gatech.edu>
2026-09-07 12:25:19 -0400

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        // since we can only consume one bin per hour, the max k should be max_of(piles)
+        // koko needs to do smoething per bin so the min k is one
+        // find minimum valid k binary searching this space
+        // a valid k -> try something smaller
+        // invlid k -> try something larger 
+
+        int hi = INT_MIN;
+        for (auto p : piles) {
+            hi = max(hi, p);
+        }
+        
+        int lo = 1;
+
+        // the binary search bound `lo` is a proxy for the solution k at the end
+        // bc even if we get a valid mid, it goes not gaurantee optimality
+
+        while (lo <= hi) {
+            int mid = lo + (hi-lo) / 2;
+
+            // check validity
+            long long time = 0;
+            for (auto p : piles) {
+                time += (p + mid - 1LL) / mid;
+            }
+
+            if (time <= h) {
+                hi = mid-1;
+            } else {
+                lo = mid+1;
+            }
+        }
+
+        return lo;
+
+        
+    }
+};
