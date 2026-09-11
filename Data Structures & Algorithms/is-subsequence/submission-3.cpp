51a3b2ef17273c1ada927aa2c671a64ca9bd3261
Udit8348 <usubramanya3@gatech.edu>
2026-09-10 22:45:37 -0400

    Add: is-subsequence - submission-3

---
+++ Data Structures & Algorithms/is-subsequence/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        if (n > m) return false;
+
+        int i = 0;
+        for (int j = 0; j < m; j++) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+
+            if (i == n) break; // short circuit when we match s in its entirity
+        }
+
+        return i == n;
+    }
+};
